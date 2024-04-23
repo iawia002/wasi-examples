d8c7ee8cd6798ddf7b3aa515a4382f92c8788c27
Xinzhao Xu <z2d@jifangcheng.com>
2024-04-23 11:21:39 +0800

    cli: use clap to parse args

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -3,14 +3,25 @@ mod bindings;
 
 use bindings::exports::wasi::cli::run::Guest;
 use bindings::wasi::cli::environment::{get_arguments, get_environment};
+use clap::Parser;
 
 struct Component;
 
 bindings::export!(Component with_types_in bindings);
 
+#[derive(Parser, Debug)]
+struct Args {
+    /// Name
+    #[arg(short, long)]
+    name: String,
+}
+
 impl Guest for Component {
     fn run() -> Result<(), ()> {
         println!("args: {:?}\nenvs: {:?}", get_arguments(), get_environment());
+
+        let args = Args::parse();
+        println!("name: {}", args.name);
         Ok(())
     }
 }
