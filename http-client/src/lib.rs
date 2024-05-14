a3f363ed4ebf6a01e08b199d34e17b4a61495199
Xinzhao Xu <z2d@jifangcheng.com>
2024-05-14 11:31:18 +0800

    http-client: add more usage examples

--- http-client/src/lib.rs
+++ http-client/src/lib.rs
@@ -11,17 +11,73 @@ bindings::export!(Component with_types_in bindings);
 
 impl Guest for Component {
     fn run() -> Result<(), ()> {
+        // get with query
+        let resp = Client::new()
+            .get("https://httpbin.org/get?a=b")
+            .send()
+            .unwrap();
+        println!(
+            "GET https://httpbin.org/get, status code: {}, body:\n{}",
+            resp.status(),
+            String::from_utf8_lossy(resp.body())
+        );
+
+        // post with json data
         let resp = Client::new()
             .post("https://httpbin.org/post")
-            .body("hello".as_bytes())
+            .header("Content-Type", "application/json")
+            .unwrap()
+            .body("{\"data\": \"hello\"}".as_bytes())
             .connect_timeout(Duration::from_secs(5))
             .send()
             .unwrap();
-        println!("status code: {}", resp.status());
-        for (key, value) in resp.headers() {
-            println!("{key}: {value}");
-        }
-        println!("body:\n{}", String::from_utf8(resp.body().clone()).unwrap());
+        println!(
+            "POST https://httpbin.org/post, status code: {}, body:\n{}",
+            resp.status(),
+            String::from_utf8_lossy(resp.body())
+        );
+
+        // post with form data
+        let resp = Client::new()
+            .post("https://httpbin.org/post")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .unwrap()
+            .body("a=b&c=".as_bytes())
+            .connect_timeout(Duration::from_secs(5))
+            .send()
+            .unwrap();
+        println!(
+            "POST https://httpbin.org/post, status code: {}, body:\n{}",
+            resp.status(),
+            String::from_utf8_lossy(resp.body())
+        );
+
+        // post with file form data
+        let resp = Client::new()
+            .post("https://httpbin.org/post")
+            .header("Content-Type", "multipart/form-data; boundary=boundary")
+            .unwrap()
+            .body(
+                "--boundary
+Content-Disposition: form-data; name=field1
+
+value1
+--boundary
+Content-Disposition: form-data; name=field2; filename=file.txt
+Content-Type: text/plain
+
+hello
+--boundary--"
+                    .as_bytes(),
+            )
+            .connect_timeout(Duration::from_secs(5))
+            .send()
+            .unwrap();
+        println!(
+            "POST https://httpbin.org/post, status code: {}, body:\n{}",
+            resp.status(),
+            String::from_utf8_lossy(resp.body())
+        );
         Ok(())
     }
 }
